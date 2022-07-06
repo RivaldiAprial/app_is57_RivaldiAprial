c0620132e8de428531acb1c8e55fe2665639b744
RivaldiAprial <107973382+RivaldiAprial@users.noreply.github.com>
2022-07-06 11:11:47 +0700

    membuat database penduduk dan gampong

---
+++ database/migrations/2022_07_06_035130_create_penduduks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('penduduks', function (Blueprint $table) {
+            $table->id();
+            $table->string('nik')->unique();
+            $table->string('nama');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->string('jenis_kelamin');
+            $table->string('agama');
+            $table->string('alamat');
+            $table->string('hp');
+            $table->bigInteger('gampongs_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('penduduks');
+    }
+};
