31b472094aeeb673e6c5659fd9e230a4a75309f9
RivaldiAprial <107973382+RivaldiAprial@users.noreply.github.com>
2022-07-27 09:37:35 +0700

    perbaiki halaman master

--- resources/views/penduduk/form.blade.php
+++ resources/views/penduduk/form.blade.php
@@ -1,15 +1,42 @@
 @extends('layouts.master')
 
 @section('content')
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+        <div class="col-sm-6">
+            <h1>Form Tambah Penduduk</h1>
+        </div>
+        <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Form Tambah Penduduk</li>
+            </ol>
+        </div>
+        </div>
+    </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+    <!-- Default box -->
     <div class="card">
-      <div class="card-header">
-        <div class="card card-primary">
-          <div class="card-header">
-            <h3 class="card-title">FORM TAMBAH PENDUDUK</h3>
-          </div>
-          <!-- /.card-header -->
-          <!-- form start -->
-          <form method="post" action="/penduduk/store">
+        <div class="card-header">
+
+        <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+            <i class="fas fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+            <i class="fas fa-times"></i>
+            </button>
+        </div>
+        </div>
+        <div class="card-body table-responsive p-0">
+          <form class="form-horizontal" action="/penduduk/store" method="POST">
             @csrf
             <div class="card-body">
               <div class="form-group">
--- resources/views/penduduk/index.blade.php
+++ resources/views/penduduk/index.blade.php
@@ -2,39 +2,48 @@
 
 @section('content')
 <div class="content-wrapper">
-  <!-- Content Header (Page header) -->
-  <section class="content-header">
-  <div class="container-fluid">
-      <div class="row mb-2">
-      <div class="col-sm-6">
-          <h1>Data Penduduk</h1>
-      </div>
-      <div class="col-sm-6">
-          <ol class="breadcrumb float-sm-right">
-          <li class="breadcrumb-item"><a href="#">Home</a></li>
-          <li class="breadcrumb-item active">Data Penduduk</li>
-          </ol>
-      </div>
-      </div>
-  </div>
-  </section>
-  
-  <section class="content">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+        <div class="col-sm-6">
+            <h1>Data Penduduk</h1>
+        </div>
+        <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Data Penduduk</li>
+            </ol>
+        </div>
+        </div>
+    </div><!-- /.container-fluid -->
+    </section>
 
-      <!-- Default box -->
-      <div class="card">
-          <div class="card-header">
-          <a href="/penduduk/form" class="btn btn-primary">Tambah Data</a>
-          </div>
-          <div class="card-body table-responsive p-0">
-                  <table class="table table-hover text-nowrap">
+    <!-- Main content -->
+    <section class="content">
+
+    <!-- Default box -->
+    <div class="card">
+        <div class="card-header">
+        <a href="/penduduk/form" class="btn btn-primary">Tambah Data</a>
+
+        <div class="card-tools">
+            <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+            <i class="fas fa-minus"></i>
+            </button>
+            <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+            <i class="fas fa-times"></i>
+            </button>
+        </div>
+        </div>
+        <div class="card-body table-responsive p-0">
+                <table class="table table-hover text-nowrap">
                   <thead>
                       <tr>
                       <th>NO</th>
                       <th>NIK</th>
                       <th>NAMA</th>
-                      <th>TEMPAT LAHIR</th>
-                      <th>TANGGAL LAHIR</th>
+                      <th>TEMPAT/TGL LAHIR</th>
                       <th>JENIS KELAMIN</th>
                       <th>AGAMA</th>
                       <th>ALAMAT</th>
@@ -49,8 +58,7 @@
                         <th scope="row">{{$nomor++}}</th>
                           <td>{{$item->nik}}</td>
                           <td>{{$item->nama}}</td>
-                          <td>{{$item->tempat_lahir}}</td>
-                          <td>{{$item->tanggal_lahir}}</td>
+                          <td>{{$item->tempat_lahir}}/{{date_format(date_create($item->tanggal_lahir),'d-M-Y')}}</td>
                           <td>{{$item->jenis_kelamin}}</td>
                           <td>{{$item->agama}}</td>
                           <td>{{$item->hp}}</td>
@@ -58,31 +66,32 @@
                           <td>{{$item->gampongs->nama_gampong}}</td>
                           
                           <td>
-                              <a href="#" class="btn btn-info">Edit</a>
-                              <button type="button" class="btn btn-danger btn--sm" data-bs-toggle="modal" data-bs-target="#a{{$item->id}}">
-                                HAPUS
-                              </button>
-                              <div class="modal fade text-dark" id="a{{$item->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                <div class="modal-dialog">
-                                  <div class="modal-content">
-                                    <div class="modal-header">
-                                      <h5 class="modal-title" id="exampleModalLabel">Peringatan</h5>
-                                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                      Yakin Atas Nama {{$item->nama}} Ingin Dihapus?
-                                    </div>
-                                    <div class="modal-footer">
-                                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Batal</button>
-                                      <form method="POST" action="penduduk/{{$item->id}}">
-                                        @csrf
-                                        @method('DELETE')
-                                      <button type="submit" class="btn btn-primary">Hapus</button>
-                                      </form>
+                            <a href="/penduduk/edit/{{$item->id}}" class="btn btn-sm text-white btn-info"><i class="fa fa-pencil-alt"></i></a>
+                                <button type="button" class="btn btn-sm btn-danger" data-toggle="modal" data-target="#a{{$item->id}}">
+                                  <i class="fa fa-trash-alt"></i>
+                                </button>
+                                
+                                <!-- Modal -->
+                                <div class="modal fade" id="a{{$item->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                  <div class="modal-dialog">
+                                    <div class="modal-content">
+                                      <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Peringatan</h5>
+                                      </div>
+                                      <div class="modal-body">
+                                        Yakin ingin menghapus nama <b>{{$item->nama}}</b> ?
+                                      </div>
+                                      <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                                        <form method="POST" action="/penduduk/{{$item->id}}">
+                                          @csrf
+                                          @method('DELETE')
+                                          <button type="submit" class="btn btn-primary">Hapus</button>
+                                        </form>
+                                      </div>
                                     </div>
                                   </div>
                                 </div>
-                              </div>
                           </td>
                       </tr> 
                       @empty
